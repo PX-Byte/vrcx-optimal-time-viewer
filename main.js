90e260777747be35c2eb972a11e4cd6882a6d918
RubenPX <rubenmarchante@gmail.com>
2023-02-23 00:09:42 +0100

    ✨ Parse data

--- main.js
+++ main.js
@@ -32,6 +32,34 @@ function inputFileRecived(files) {
  * @param {string} data 
  */
 function readData(data) {
+  console.log(data)
+
+  // replace windows crlf to unix lf
+  data = data.replace("\r\n", "\n");
+
+  if (data.slice(0, 6) != "bucket") {
+    alert("Invalid input file");
+  }
+
+  // separate by lines and columns by \t --> https://github.com/zkxs/vrcx-optimal-time/blob/5033331996224321dbf231791e055aaa5b06f20d/src/main.rs#L312
+  let parsedData = data.split("\n").map((ln) => ln.split("\t"))
+
+  /** DATA FORMATING FOR HEADERS */
+
+  // Get times list
+  let times = parsedData.map((value, index) => value[0]);
+  times.shift(); // Remove first element (csv header)
+
+  // Get days
+  let days = parsedData[0];
+  days.shift();
+
+  /** Chart builder */
+
+  
+
+  
+  
 
   debugger;
   
